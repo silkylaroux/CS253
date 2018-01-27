18960561e4fdb9c65afdb978c7d92615c6ee4eb9
Unknown <damian.mikayla@gmail.com>
2018-01-27 16:50:28 -0700

    Working code
    It works for test cases given, just need to clean and comment, and check fringe cases

--- HW1/hw1.cc
+++ HW1/hw1.cc
@@ -1,26 +1,48 @@
 #include <iostream>
 #include <iomanip>
-
+using namespace std;
 
 void printNum(int input){
-if(input >= -8 and input < 8){
-		std::cout << input<< ": " << std::hex << (input & 0x0f) << '\n';
+
+	if(input >= -8 and input < 8){
+		cout << input<< ": " << setfill('0') << setw(2) << hex << ((input & 0x0f)+(0x00 << 1)) 
+		<< dec << '\n';
+	
 	} else if(input >= -2048 and input < 2048){
-		std::cout << input << ": " << std::hex << ((input & 0x0fff) + (0x1 << 3)) << '\n';
+		cout << input << ": " << hex << (((input & 0x0f00) + (0x1 << 12)) >> 8) 
+		<< " " << (input & 0x00ff) << dec << '\n';
+	
 	} else if (input >= -524288 and input < 524288){
-		std::cout << input << ": " << std::hex << ((input & 0x0fffff) + 0x200000) << '\n';
+		cout << input << ": " << hex << (((input & 0x0f0000) + (0x2 << 20))>>16) 
+		<< " " << ((input & 0xff00)>>8) << " " << (input & 0xff) << dec << '\n';
+	
 	} else if (input >= -134217728 and input < 134217728){
-		std::cout << input << ": " << std::hex << ((input & 0x0fffffff) + 0x30000000) << '\n';
+		cout << input << ": " << hex << (((input & 0x0f000000) + (0x3 << 28))>>24)
+		<< " " << ((input & 0xff0000)>>16)<<" " << ((input & 0xff00)>>8) << " " << (input & 0xff) 
+		<< dec << '\n';
+	
 	} else {
-		std::cout << input << ": " << std::hex << ((input & 0x0fffffffff) + 0x4000000000) << '\n';
+		cout << input << ": " << hex << (((input & 0x0f00000000) + (0x4000000000))>>32)
+		<< " " << ((input & 0xff000000)>>24)<< " " << ((input & 0xff0000)>>16)<< " " 
+		<< ((input & 0xff00)>>8) << " " << setw(2) << (input & 0xff) << dec << '\n';
 	} 
+
 }
 
 
 int main(){
-	int input;
-	std::cin >> input;
-	printNum(input);
+
+	int input1;
+
+    while (cin >> input1) {
+
+        printNum(input1);
+        
+    }
+
+    if (!cin.eof())
+        cerr << "Read failed, but not at eof.  Must be non-numeric data.\n";
+
 	return 0;
 }
 
