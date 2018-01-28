b5b2b41f3cbaac58ee78dcc806c3aeca4f6433c8
Unknown <damian.mikayla@gmail.com>
2018-01-27 17:31:25 -0700

    Done
    added comments and extra checks, not the best but it is done

--- HW1/hw1.cc
+++ HW1/hw1.cc
@@ -3,6 +3,11 @@
 using namespace std;
 
 void printNum(int input){
+	// Each if statement is checking to see how many bytes are needed
+	// Then it will in the cout(badcoding) go through 1 byte at a time
+	// separate and space out the hex values.
+	// It does this by creating a mask of 0xff00... and bit shifting 
+	// to desired 8 bit length
 
 	if(input >= -8 and input < 8){
 		cout << input<< ": " << setfill('0') << setw(2) << hex << ((input & 0x0f)+(0x00 << 1)) 
@@ -10,38 +15,44 @@ void printNum(int input){
 	
 	} else if(input >= -2048 and input < 2048){
 		cout << input << ": " << hex << (((input & 0x0f00) + (0x1 << 12)) >> 8) 
-		<< " " << (input & 0x00ff) << dec << '\n';
+		<< " " << setw(2) << (input & 0x00ff) << dec << '\n';
 	
 	} else if (input >= -524288 and input < 524288){
 		cout << input << ": " << hex << (((input & 0x0f0000) + (0x2 << 20))>>16) 
-		<< " " << ((input & 0xff00)>>8) << " " << (input & 0xff) << dec << '\n';
+		<< " " << setw(2) << ((input & 0xff00)>>8) << " " << setw(2) 
+		<< (input & 0xff) << dec << '\n';
 	
 	} else if (input >= -134217728 and input < 134217728){
 		cout << input << ": " << hex << (((input & 0x0f000000) + (0x3 << 28))>>24)
-		<< " " << ((input & 0xff0000)>>16)<<" " << ((input & 0xff00)>>8) << " " << (input & 0xff) 
-		<< dec << '\n';
+		<< " " << setw(2) << ((input & 0xff0000)>>16)<< " " << setw(2) 
+		<< ((input & 0xff00)>>8) << " " << setw(2) << (input & 0xff) << dec << '\n';
 	
 	} else {
 		cout << input << ": " << hex << (((input & 0x0f00000000) + (0x4000000000))>>32)
-		<< " " << ((input & 0xff000000)>>24)<< " " << ((input & 0xff0000)>>16)<< " " 
-		<< ((input & 0xff00)>>8) << " " << setw(2) << (input & 0xff) << dec << '\n';
+		<< " " << setw(2) << ((input & 0xff000000)>>24)<< " " << setw(2) 
+		<< ((input & 0xff0000)>>16)<< " " << setw(2) << ((input & 0xff00)>>8) 
+		<< " " << setw(2) << (input & 0xff) << dec << '\n';
 	} 
 
 }
 
-
+// This program will take in the decimal value and turn it into a hex vnum value 
+// ie. it will turn it into a hex number with the first nibble being the length of
+// additional bytes. To stop the program it either has to reach the end of the file, or
+// you terminate it (ctrl-d in linux)
 int main(){
 
 	int input1;
 
     while (cin >> input1) {
-
-        printNum(input1);
-        
+        printNum(input1);        
     }
 
-    if (!cin.eof())
+    if (!cin.eof()){
         cerr << "Read failed, but not at eof.  Must be non-numeric data.\n";
+    }
+
+	
 
 	return 0;
 }
