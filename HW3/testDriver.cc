60e0243ebc8aaddb38c009074df3358d519b5ab7
Unknown <damian.mikayla@gmail.com>
2018-02-23 03:07:34 -0700

    works to unserialize chars from file
    check the testDriver.cc need to put in unserparser.cc

--- HW3/testDriver.cc
+++ HW3/testDriver.cc
@@ -10,23 +10,99 @@
 #include <stdlib.h>
 
 using namespace std;
-/*int main(){
-    string str = "3 400 -222 ";
-    vector<string> s = parse(str);
+int main(){
+    //string input = "73017302730373046321";
+    string input = "632163666369";
+    vector<string> s;
+    if((input.length()%2)!=0){
+        cerr << " From [runner.cc]: not valid input unequal amount of hex values";
+        exit(1); 
+    }else{
+        string token_holder = input;
+        //string check_sign = "";
+        //for(int i; token_holder.length()>0;i++){
+        bool not_zero = true;
+        while(not_zero){
+            string tag = token_holder.substr(0,2);
+            //token_holder == token_holder.substr(2);
+            //cout << token_holder.length() << "tag " << tag << '\n';
+            /*if(tag.compare("74")==0){                       // check if true
+                s.push_back("t");
+                //token_holder == token_holder.substr(2);
 
-    vector<Vnum> vnum_vec;
-    for(auto st: s){
-        Vnum num(st);
-        vnum_vec.push_back(num);
+            } else if(tag.compare("66")==0){                // check if false
+                s.push_back("f");
+                //token_holder == token_holder.substr(2);
+
+            } else if(tag.compare("69")==0 || tag.compare("73") == 0 || tag.compare("6c")==0){
+                if(token_holder.at(0)=='0'){
+                    
+                    s.push_back(token_holder.substr(1,1)); // just return the second hex value
+                    //token_holder == token_holder.substr(2);
+                }else{
+                    //token_holder == token_holder.substr(2);
+                    char str1 = token_holder.at(0);
+                    stringstream ss;
+                    ss << str1;
+                    unsigned int len;
+                    ss >> len;
+                    if(token_holder.length()>= (len * 2)){
+                        //int i = 0;
+                        //for(unsigned int x = 2; x<len; x= x+2){
+                        //    stringstream hex_check;
+                        //    hex_check << hex << token_holder.substr(i,x);
+                        //}
+                        stringstream ss;
+                        ss << token_holder.substr(1,(len*2));
+                        //cout << ss.str() << '\n';
+                        token_holder = token_holder.substr(len*2);
+                    }
+                    token_holder = token_holder.substr(2);
+                }
+            } else*/ if(tag.compare("63")==0){                                           // if char
+                token_holder = token_holder.substr(2);
+                //cout << "tag compare: " << tag << " num "<< tag.compare("63")<<'\n';
+                if(tag.compare("63")==0){
+                    //cout << token_holder <<'\n';
+                    string str1 = token_holder.substr(0,2);
+                    //cout << str1 << '\n';
+                    stringstream ss;
+                    ss << hex << str1;
+                    unsigned int x;
+                    ss >> x; 
+                    char c = x;
+                    //ss.str("");
+                    //ss << c;
+                    string holder;
+                    holder.push_back(c);
+                    //cout << c<< holder << '\n';
+                    s.push_back(holder);
+                    //cout << "token hold: "<< token_holder <<'\n';
+                                
+                }
+            }else{
+                token_holder == token_holder.substr(2);
+            }
+            //break;
+            token_holder.erase(0,2);// == token_holder.substr(1);
+            if(token_holder.length() <= 0){
+                not_zero = false;
+            }
+            //cout << token_holder.length();
+            //break;
+        }
+    for(auto ch:s){
+        
+        cout << "vector test " << ch << '\n';
     }
-    for(auto p: vnum_vec){
-        cout << p.get_vnum();
-    }
-    
     //cout << num.get_vnum();
     //s.push_back(num);
+    //return 0;
+    }
     return 0;
-} */
+} 
+
+/****************
 void run_serialize(int opt, int args_int, char **args_names){
     int holder = opt;
     while(holder <= args_int){
@@ -70,6 +146,8 @@ int main(int argc, char **argv){
                     break; 
             }
         }
+    ********************/
+
     /*(c = getopt (argc, argv, "usv:"));
     int holder = optind;
     while(holder <= argc){
@@ -115,8 +193,9 @@ int main(int argc, char **argv){
                     print_vector(tokens);           // From printer.cc This prints the vector of vnum strings   
                 }
             } 
-        }*/ 
+        } 
     } 
      
     return 0; 
 }   
+*/
--- HW3/unserParser.cc
+++ HW3/unserParser.cc
@@ -12,7 +12,7 @@
 #include "unserParser.h"
 using namespace std;
 
-bool is_hex(char ch){
+bool is_hex_char(char ch){
     char small_ch = tolower(ch);
     switch(small_ch){
         case 'a':
@@ -32,22 +32,44 @@ bool is_hex(char ch){
     }
 
 }
+bool check_tag(string str){
+
+}
 
 string unserial_parse(string input){
+    vector<string> s;
     if((input.length()%2)!=0){
         cerr << " From [runner.cc]: not valid input unequal amount of hex values";
         exit(1); 
     }else{
         string token_holder = input;
         string check_sign = "";
+
+        while(token_holder.length()>0){
+            string tag = token_holder.substr(0,2);
+            if(tag.compare("66")==0 || tag.compare("74")){
+                s.push_back(tag);
+                token_holder = token_holder.substr(3);
+            }else{
+                s.push_back(tag);
+                token_holder = token_holder.substr(3);
+                int num;
+                num << hex <<
+            }
+
+        }
+
+
+
         for(unsigned int x =0; x < token_holder.length(); x++){
             char token_char = token_holder.at(x);
 
-            if(!isdigit(token_char) && !is_hex(token_char)){
-                cerr <<" From [runner.cc]: not valid input "<< token_char << "is not valid hex value\n";
+            if(!isdigit(token_char) && !is_hex_char(token_char)){
+                cerr <<" From [unserParser.cc]: not valid input "<< token_char << "is not valid hex value\n";
                 exit(0);
             }else if(check_sign.length() == 1){
                 check_sign = check_sign + token_char;
+                if()
             }
         }
     return input;
